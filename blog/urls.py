c9ef821002cad9daa58fcc6c5b5f6436bb9d8ee4
NJ <r_curtain@163.com>
2018-11-17 19:09:05 +0800

    10.使用CSS美化页面

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'blog'
 urlpatterns = [
+    path('', views.blog_list, name="blog_list"),
     path('<int:blog_pk>', views.blog_detail, name="blog_detail"),
     path('type/<int:blog_type_pk>', views.blogs_with_type, name="blogs_with_type"),
 ]
