849492982337bd627f40b9af65090ddaa429a060
NJ <r_curtain@163.com>
2018-11-17 21:34:10 +0800

    12.Bootstrap响应式布局

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .models import Blog, BlogType
 def blog_list(request):
     context = {}
     context['blogs'] = Blog.objects.all()
-    context['blogs_count'] = Blog.objects.all().count()
+    context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog/blog_list.html', context)
 
 
@@ -19,5 +19,5 @@ def blogs_with_type(request, blog_type_pk):
     context = {}
     blog_type = get_object_or_404(BlogType, id=blog_type_pk)
     context['blogs'] = Blog.objects.filter(blog_type=blog_type)
-    context['blog_type'] = blog_type
-    return render_to_response('blog/blog_with_type.html', context)
+    context['blog_types'] = BlogType.objects.all()
+    return render_to_response('blog/blogs_with_type.html', context)
