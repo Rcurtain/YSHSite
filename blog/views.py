cd558ce34d7b0e2df556ad17398f30cd5d9e31b1
NJ <r_curtain@163.com>
2018-11-18 15:11:59 +0800

    16.博客分类统计

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from .models import Blog, BlogType
+from django.db.models import Count
 from django.core.paginator import Paginator
 from django.conf import settings
 
@@ -21,12 +22,27 @@ def get_blog_list_common_date(request, blogs_all_list):
     if page_range[-1] != paginator.num_pages:
         page_range.append(paginator.num_pages)
 
+    #获取博客分类对应的数量
+    # blog_types = BlogType.objects.all()
+    # blog_types_list = []
+    # for blog_type in blog_types:
+    #     blog_type.blog_count = Blog.objects.filter(blog_type=blog_type).count()
+    #     blog_types_list.append(blog_type)
+
+    #获取日期归档相应的博客数量
+    blog_dates = Blog.objects.dates('create_time', 'month', order="DESC")
+    blog_dates_dict = {}
+    for blog_date in blog_dates:
+        blog_count = Blog.objects.filter(create_time__year = blog_date.year,
+                                        create_time__month = blog_date.month).count()
+        blog_dates_dict[blog_date] = blog_count
+
     context = {}
     context['blogs'] = page_of_blogs.object_list
     context['page_of_blogs'] = page_of_blogs
     context['page_range'] = page_range
-    context['blog_types'] = BlogType.objects.all()
-    context['blog_dates'] = Blog.objects.dates('create_time', 'month', order="ASC")
+    context['blog_types'] = BlogType.objects.annotate(blog_count = Count('blog'))
+    context['blog_dates'] = blog_dates_dict
     return context
 
 # Create your views here.
