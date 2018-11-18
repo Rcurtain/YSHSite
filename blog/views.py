3a943ef281d51f530b559fd282fdc3efabbe4c4b
NJ <r_curtain@163.com>
2018-11-18 11:23:14 +0800

    14.优化分页展示

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,32 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from .models import Blog, BlogType
 from django.core.paginator import Paginator
+from django.conf import settings
 
 # Create your views here.
 def blog_list(request):
     blogs_all_list = Blog.objects.all()
-    paginator = Paginator(blogs_all_list, 10)#
+    paginator = Paginator(blogs_all_list, settings.EACH_PAGE_BLOGS_NUMBER)#
     page_num = request.GET.get("page", 1)#get page num
     page_of_blogs = paginator.get_page(page_num)
+    current_page_num = page_of_blogs.number
+    page_range = [current_page_num - 2, current_page_num - 1, current_page_num, current_page_num + 1, current_page_num + 2]
+    page_range = list(range(max(current_page_num - 2, 1), current_page_num)) + \
+                 list(range(current_page_num, min(current_page_num + 2, paginator.num_pages) + 1))
+    if page_range[0] - 1 >= 2:
+        page_range.insert(0, "...")
+    if paginator.num_pages - page_range[-1] >= 2:
+        page_range.append("...")
+    # 加上首页和尾页
+    if page_range[0] != 1:
+        page_range.insert(0, 1)
+    if page_range[-1] != paginator.num_pages:
+        page_range.append(paginator.num_pages)
 
     context = {}
+    context['blogs'] = page_of_blogs.object_list
     context['page_of_blogs'] = page_of_blogs
+    context['page_range'] = page_range
     context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog/blog_list.html', context)
 
