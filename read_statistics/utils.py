3d360c3135064665ded7bd0b833473484d87f660
NJ <r_curtain@163.com>
2018-11-19 18:28:00 +0800

    20.阅读计数统计和显示 使用了highcharts，并且使用了model中的aggregate方法

--- read_statistics/utils.py
+++ read_statistics/utils.py
@@ -1,18 +1,35 @@
+import datetime
+from django.utils import timezone
 from django.contrib.contenttypes.models import ContentType
-from .models import ReadNum
+from .models import ReadNum, ReadDetail
+from django.db.models import Sum
 
 def read_statistics_once_read(request, obj):
     ct = ContentType.objects.get_for_model(obj)
     key = "%s_%s_read" % (ct.model, obj.pk)
 
     if not request.COOKIES.get(key):
-        if ReadNum.objects.filter(content_type=ct, object_id=obj.pk).count():
-            # 存在记录
-            readnum = ReadNum.objects.get(content_type=ct, object_id=obj.pk)
-        else:
-            # 不存在对应的记录
-            readnum = ReadNum(content_type=ct, object_id=obj.pk)
+        readnum, created = ReadNum.objects.get_or_create(content_type=ct, object_id=obj.pk)
         # 计数加1
         readnum.read_num += 1
         readnum.save()
+
+        date = timezone.now().date()
+        readdetail, created = ReadDetail.objects.get_or_create(content_type=ct, object_id=obj.pk, date=date)
+        readdetail.read_num += 1
+        readdetail.save()
+
     return key
+
+
+def get_seven_days_read_data(content_type):
+    today = timezone.now().date()
+    dates = []
+    read_nums = []
+    for i in range(7, 0, -1):
+        date = today - datetime.timedelta(days=i)
+        dates.append(date.strftime('%m/%d'))
+        read_details = ReadDetail.objects.filter(content_type=content_type, date=date)
+        result = read_details.aggregate(read_num_sum=Sum('read_num'))
+        read_nums.append(result['read_num_sum'] or 0)
+    return dates, read_nums
