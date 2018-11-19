d0e9661bf44350c406bbbb63e7c978d699311836
NJ <r_curtain@163.com>
2018-11-19 17:56:49 +0800

    19.博客分类计数功能 使用django的contenttype功能

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render_to_response, get_object_or_404
-from .models import Blog, BlogType
-from django.db.models import Count
 from django.core.paginator import Paginator
 from django.conf import settings
+from django.db.models import Count
+from .models import Blog, BlogType
+from read_statistics.utils import read_statistics_once_read
 
-def get_blog_list_common_date(request, blogs_all_list):
+def get_blog_list_common_data(request, blogs_all_list):
     paginator = Paginator(blogs_all_list, settings.EACH_PAGE_BLOGS_NUMBER)#
     page_num = request.GET.get("page", 1)#get page num
     page_of_blogs = paginator.get_page(page_num)
@@ -48,27 +49,30 @@ def get_blog_list_common_date(request, blogs_all_list):
 # Create your views here.
 def blog_list(request):
     blogs_all_list = Blog.objects.all()
-    context = get_blog_list_common_date(request, blogs_all_list)
+    context = get_blog_list_common_data(request, blogs_all_list)
     return render_to_response('blog/blog_list.html', context)
-
-
-def blog_detail(request, blog_pk):
-    context = {}
-    blog = get_object_or_404(Blog, pk=blog_pk)
-    context['blog'] = get_object_or_404(Blog, pk=blog_pk)
-    context['previous_blog'] = Blog.objects.filter(create_time__gt=blog.create_time).last()
-    context['next_blog'] = Blog.objects.filter(create_time__lt=blog.create_time).first()
-    return render_to_response('blog/blog_detail.html', context)
-
 
 def blogs_with_type(request, blog_type_pk):
     blog_type = get_object_or_404(BlogType, id=blog_type_pk)
     blogs_all_list = Blog.objects.filter(blog_type=blog_type)
-    context = get_blog_list_common_date(request, blogs_all_list)
+    context = get_blog_list_common_data(request, blogs_all_list)
+    context['blog_type'] = blog_type
     return render_to_response('blog/blogs_with_type.html', context)
 
 def blogs_with_date(request, year, month):
     blogs_all_list = Blog.objects.filter(create_time__year=year, create_time__month=month)
-    context = get_blog_list_common_date(request, blogs_all_list)
+    context = get_blog_list_common_data(request, blogs_all_list)
     context['blogs_with_date'] = "%s年%s月" %(year, month)
     return render_to_response('blog/blogs_with_date.html', context)
+
+def blog_detail(request, blog_pk):
+    blog = get_object_or_404(Blog, pk=blog_pk)
+    read_cookie_key = read_statistics_once_read(request, blog)
+
+    context = {}
+    context['previous_blog'] = Blog.objects.filter(create_time__gt=blog.create_time).last()
+    context['next_blog'] = Blog.objects.filter(create_time__lt=blog.create_time).first()
+    context['blog'] = blog
+    response = render_to_response('blog/blog_detail.html', context) # 响应
+    response.set_cookie(read_cookie_key, 'true') # 阅读cookie标记
+    return response
