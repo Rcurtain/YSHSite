3eaa2da6a260c15238ce0a7da99834f6de4c21fc
NJ <r_curtain@163.com>
2018-11-24 11:29:42 +0800

    24.使用Django Form表单

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import home, login
+from .views import home, login, register
 
 urlpatterns = [
     path('', home, name='home'),
@@ -25,7 +25,8 @@ urlpatterns = [
     path('ckeditor/', include("ckeditor_uploader.urls")),
     path('blog/', include("blog.urls")),
     path('comment/', include("comment.urls")),
-    path('login/', login, name='login')
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
