f32243ab316515d725cdb4453ebeac3aa855d394
NJ <r_curtain@163.com>
2018-12-16 15:44:59 +0800

    32.修改用户信息，姓名，邮箱等待

--- mysite/settings.py
+++ mysite/settings.py
@@ -182,3 +182,14 @@ CACHES = {
         'LOCATION': 'my_cache_table',
     }
 }
+
+# 发送邮件设置
+# https://docs.djangoproject.com/en/2.0/ref/settings/#email
+# https://docs.djangoproject.com/en/2.0/topics/email/
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.qq.com'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = '1399195602@qq.com'
+EMAIL_HOST_PASSWORD = 'happy2011'  # 授权码
+EMAIL_SUBJECT_PREFIX = '[NJ的博客] '
+EMAIL_USE_TLS = True  # 与SMTP服务器通信时，是否启动TLS链接(安全链接)
